0c9de8948ac3a6369d0002bdf95f3bf05211a7a0
Petrel321 <ivan.sannikov20042@mail.ru>
2024-10-03 23:00:07 +0300

    fix video save

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 import os
+import time
+import hashlib
 from video_stream import start_video_stream
+
+def generate_unique_filename(base_name="processed_output"):
+    current_time = time.time()
+    hash_object = hashlib.md5(str(current_time).encode()) 
+    unique_hash = hash_object.hexdigest()
+    return f"{base_name}_{unique_hash}.mp4"
 
 if __name__ == "__main__":
     # video_path = "inputs/input-1.mp4"  # Path to the input video file
     video_path = 0  # Using the camera
     
-    output_path = "outputs/processed_output.mp4"  # Path to save the output video
+    output_path = os.path.join("outputs", generate_unique_filename())  # Path to save the output video
+    os.makedirs(os.path.dirname(output_path), exist_ok=True) # create folder if not exists
 
     # Check if the video file exists
     if not os.path.exists(video_path):
--- src/video_stream.py
+++ src/video_stream.py
@@ -1,5 +1,4 @@
 import cv2
-import time
 from filters import apply_sepia, apply_grayscale, apply_mirror, apply_resize
 
 def apply_filters(frame):
@@ -15,25 +14,28 @@ def start_video_stream(video_path, output_path, duration=10):
         print(f"Error: Cannot open video source {video_path}")
         return
 
+    # Settings for recording video
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS) or 20.0
+
     # Options for recording video
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(output_path, fourcc, 20.0, (640, 480))
+    out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
 
-    start_time = time.time()
-
-    while True:
+    while(cap.isOpened()):
         ret, frame = cap.read()
-        if not ret or (time.time() - start_time) > duration:
-            print("Video processing stopped after duration or end of file.")
-            break
+        if ret==True:
+            # write the flipped frame
+            processed_frame = apply_grayscale(frame)
+            processed_frame = cv2.resize(processed_frame, (frame_width, frame_height))
+            out.write(processed_frame)
+            cv2.imshow("Processed Video", processed_frame)
 
-        processed_frame = apply_filters(frame)
-        out.write(processed_frame)
-        cv2.imshow("Processed Video", processed_frame)
-
-        # Interrupt by pressing the ‘q’ key
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            print("Video stopped by user pressing 'q'.")
+            # Interrupt by pressing the ‘q’ key
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
             break
 
     cap.release()
